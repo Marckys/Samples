ba5ba3051f76590f4ffb23067f937e75c6c58204
Marckys <lonetcamp@hotmail.com>
2013-10-22 15:23:02 +0200

    Add Proyects

---
+++ DemoSPA/Angular/Controllers/ConcesionariosController.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class ConcesionariosController : Controller
+    {
+        [OutputCacheAttribute(VaryByParam = "*", Duration = 0, NoStore = true)]
+        public ActionResult List()
+        {
+            ViewBag.Title = "Vista Parcial Listado";
+            return PartialView();
+        }
+        public ActionResult Detail()
+        {
+            ViewBag.Title = "Vista Parcial Detalle";
+            return PartialView();
+        }
+
+        public ActionResult Edit()
+        {
+            ViewBag.Title = "Vista Parcial Edición";
+            return PartialView();
+        }
+        public ActionResult Progress()
+        {
+            return PartialView();
+        }
+
+    }
+}
